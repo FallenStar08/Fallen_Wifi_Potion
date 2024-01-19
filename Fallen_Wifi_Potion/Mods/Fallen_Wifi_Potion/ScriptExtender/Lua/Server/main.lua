b3d09d5f3fe7e2940224de80f58eec3b054321ef
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-01-19 20:53:10 +0100

    V2 & message fix

--- Fallen_Wifi_Potion/Mods/Fallen_Wifi_Potion/ScriptExtender/Lua/Server/main.lua
+++ Fallen_Wifi_Potion/Mods/Fallen_Wifi_Potion/ScriptExtender/Lua/Server/main.lua
@@ -3,10 +3,30 @@
 local healingPotionTag = "1879a93d-2edf-4f54-85dd-81a3724d677f"
 --local stupidFuckingIteratingPieceOfShitFunctionIsDone = false
 local potions = {}
+
+local wifiPotions = {}
 -- local drinkerQueue = {}
 -- --Actually not a queue.
 -- local initQueue = {}
 
+local thrownMatches={
+    ["1119ee77-9048-427b-bd09-69911270dd01"]="small",
+    ["2f630ddb-ccea-489b-a392-95be277cd589"]="greater",
+    ["d0da8204-73a7-4684-99bd-9733986a9f56"]="superior",
+    ["7c2ef1ca-8b5a-4520-a31b-39c9d603fcb3"]="supreme"
+}
+
+--- Defines different types of healing potions.
+---@class HealingPotionType
+---@field stat string The stat associated with the healing potion type.
+---@field status string The status associated with the healing potion type.
+---@field template ROOT The template UUID associated with the WIFI healing potion type.
+---@field nameHandle string The name handle associated with the WIFI healing potion type.
+---@field descriptionPartHandle string The description part handle associated with the WIFI healing potion type.
+---@field descriptionHandle string The description handle associated with the WIFI healing potion type.
+
+--- Represents a collection of healing potion types.
+---@class HealingPotionTypes
 local healingPotionTypes = {
     ["small"] = {
         stat = "OBJ_Potion_Healing",
@@ -41,8 +61,30 @@ local healingPotionTypes = {
         descriptionHandle = "h35bcaeb6ge7eeg4853ga884g03be6d2b8d0c"
     },
 }
+
+---Function to populate our potions table by iterating over the inventory&sub inventories of each party member
+local function getPartyPotions()
+    potions = {}
+
+    for _, player in pairs(SQUADIES) do
+        potions = Table.MergeSets(potions, DeepIterateInventory(Ext.Entity.Get(player), { healingPotionTag }))
+    end
+end
+
+---Function to populate our wifiPotions table by iterating over the inventory&sub inventories of each party member
+local function findWifiPotions()
+    for _, player in pairs(SQUADIES) do
+        wifiPotions = Table.MergeSets(wifiPotions,
+            DeepIterateInventory(Ext.Entity.Get(player), nil,
+                { healingPotionTypes.small.template, healingPotionTypes.greater.template, healingPotionTypes.superior
+                    .template, healingPotionTypes.supreme.template }))
+    end
+end
+
+
+
 ---Get potion count for a potion type
----@param potionType PotionType type of potion to count the amount of
+---@param potionType HealingPotionType type of potion to count the amount of
 ---@return integer potionCount amount of the specified potion type
 local function getPotionCount(potionType)
     if type(potionType) ~= "table" then return 0 end
@@ -55,30 +97,42 @@ local function getPotionCount(potionType)
     return potionCount
 end
 
----Function to populate our potions table by iterating over the inventory&sub inventories of each party member
-local function getPartyPotions()
-    potions={}
-    for _, player in pairs(SQUADIES) do
-        potions = Table.MergeSets(potions,DeepIterateInventory(Ext.Entity.Get(player), { healingPotionTag }))
+---Update count in description & stack size for the wifi potion themselves on a potionType
+---@param potionType HealingPotionType
+local function updatePotionDescriptionAndStackSize(potionType)
+    local potionAmount = getPotionCount(potionType)
+    for uuid, data in pairs(wifiPotions) do
+        --BasicPrint({uuid=uuid,datatemp=data.template,potionT=potionType.template})
+        if data.template == potionType.template then
+            if potionAmount == 0 then
+                Osi.SetStackAmount(uuid, 1)
+            else
+                Osi.SetStackAmount(uuid, potionAmount)
+            end
+        end
+    end
+    UpdateTranslatedString(potionType.descriptionHandle,
+        tostring(GetTranslatedString(potionType.nameHandle) .. " : " .. tostring(potionAmount)))
+end
+
+---Update count in description & stack size for the wifi potion themselves for all potionTypes
+local function updateDescriptionForAllPotionTypes()
+    for k, v in pairs(healingPotionTypes) do
+        updatePotionDescriptionAndStackSize(v)
     end
 end
 
-local function updatePotionDescription(potionType)
-    UpdateTranslatedString(potionType.descriptionHandle,
-        tostring(GetTranslatedString(potionType.nameHandle) .. " : " .. tostring(getPotionCount(potionType))))
-end
-local function updateDescriptionForAllPotionTypes()
-    for k, v in pairs(healingPotionTypes) do
-        updatePotionDescription(v)
-    end
-end
-local function drinkHealingPotion(drinker, potion)
+---Consume a real healing potion (or fake it for throwing)
+---@param drinker CHARACTER The drinker to apply the related status
+---@param potion HealingPotionType
+---@param fakeDrink? boolean true to not apply the status (for throwing)
+local function drinkHealingPotion(drinker, potion, fakeDrink)
     getPartyPotions()
     local potionStat = potion.stat
     for uuid, data in pairs(potions) do
         if data.statsId == potionStat then
             if drinker then
-                Osi.ApplyStatus(drinker, potion.status, 1, 1)
+                if not fakeDrink then Osi.ApplyStatus(drinker, potion.status, 1, 1) end
                 if data.amount > 1 then
                     Osi.SetStackAmount(uuid, data.amount - 1)
                     data.amount = data.amount - 1
@@ -87,39 +141,45 @@ local function drinkHealingPotion(drinker, potion)
                     data.amount = data.amount - 1
                     potions[uuid] = nil
                 end
-                updatePotionDescription(potion)
+                updatePotionDescriptionAndStackSize(potion)
                 return
             end
         end
     end
-    updatePotionDescription(potion)
-end
-
-local function findModPotions()
-
-
+    updatePotionDescriptionAndStackSize(potion)
 end
 
 -- -------------------------------------------------------------------------- --
 --                                   EVENTS                                   --
 -- -------------------------------------------------------------------------- --
 
-Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(level, iseditor)
-    if level == "SYS_CC_I" then return end
+local function start()
+    local newWifiPotionAdded = false
+    local timeBeforeLogic = 0
     GetSquadies()
     getPartyPotions()
-    for _, player in pairs(SQUADIES) do
-        for _, item in pairs(healingPotionTypes) do
-            if not HasItemTemplate(player, item.template) then
-                Osi.TemplateAddTo(item.template, player, 1, 1)
-            end
+    for _, item in pairs(healingPotionTypes) do
+        if GiveItemToEachPartyMember(item.template) then
+            newWifiPotionAdded = true
         end
     end
+    if newWifiPotionAdded then
+        timeBeforeLogic = 666
+    end
+    DelayedCall(timeBeforeLogic, function()
+    findWifiPotions()
+    ---_D(wifiPotions)
     updateDescriptionForAllPotionTypes()
+    end)
+end
+
+Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(level, iseditor)
+    if level == "SYS_CC_I" then return end
+    start()
 end)
 
 Ext.Osiris.RegisterListener("TemplateAddedTo", 4, "after", function(root, item, inventoryHolder, addType)
-    if Osi.IsPartyMember(inventoryHolder,0) == 1 and Osi.IsTagged(GUID(item), healingPotionTag) == 1 then
+    if Osi.IsPartyMember(inventoryHolder, 0) == 1 and Osi.IsTagged(GUID(item), healingPotionTag) == 1 then
         getPartyPotions()
         updateDescriptionForAllPotionTypes()
     end
@@ -139,14 +199,19 @@ Ext.Osiris.RegisterListener("TemplateUseStarted", 3, "after", function(character
 end)
 
 Ext.Osiris.RegisterListener("CharacterJoinedParty", 1, "after", function()
-    GetSquadies()
-    for _, player in pairs(SQUADIES) do
-        for _, item in pairs(healingPotionTypes) do
-            if not HasItemTemplate(player, item.template) then
-                Osi.TemplateAddTo(item.template, player, 1, 1)
-            end
-        end
-    end
+    start()
 end)
 
 Ext.Osiris.RegisterListener("CharacterLeftParty", 1, "after", GetSquadies)
+
+Ext.Osiris.RegisterListener("OnThrown",7,"after",function(thrownObject,thrownObjectTemplate,thrower,storyActionID,x,y,z)
+    local result = thrownMatches[GUID(thrownObjectTemplate)]
+    if result then
+        drinkHealingPotion(thrower,healingPotionTypes[result],true)
+    end
+end)
+
+---Debug
+Ext.Events.ResetCompleted:Subscribe(function()
+    start()
+end)
